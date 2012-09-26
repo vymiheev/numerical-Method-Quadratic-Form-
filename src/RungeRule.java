4d3b786290828cdb906ea7b65d907ee62d861f2b
Vlad <vmiheev@Spoon.(none)>
2012-09-26 22:04:51 +0400

    Runge rule correction

--- src/RungeRule.java
+++ src/RungeRule.java
@@ -16,53 +16,77 @@ public class RungeRule {
     private final int M;
     private final double D;
     private int it = 0;
+    private int iteration = 0;
 
     public double[] runRungeRule() {
         double[] IH = new QuadratureMethod().getApproximateSolution();
         while (true) {
             EntryPoint.STEP /= STEP_DIVIDER;
             double[] Ih = new QuadratureMethod().getApproximateSolution();
-            /*findMaxDifference(Ih, IH);
-            if (findDifference(Ih, IH) < EntryPoint.EPS) {
-                return IH;
-            }*/
+            double relativeDiff = findRelativeApproximationDifference(Ih, IH);
+            System.out.println("ITERATION: " + iteration);
+            System.out.println("        Step: " + EntryPoint.STEP);
+            System.out.println("        Relative approximation difference: " + relativeDiff);
+            System.out.println("        Factual approximation difference: " + findFaсtualApproximationDifference(Ih) + "\n");
 
-            if (findMaxDifference(Ih, IH) / D < EntryPoint.EPS) {
+            if (relativeDiff < EntryPoint.EPS) {
                 return Ih;
             }
             /* if (it==2){
                 return Ih;
             }
             it++;*/
+            iteration++;
             IH = Ih.clone();
 
         }
 
     }
 
-    private double findDifference(double[] Ih, double[] IH) {
-        double IhSum = 0;
-        double IHSum = 0;
-        for (int i = 0; i < Ih.length; i += STEP_DIVIDER) {
-            IhSum += Ih[i];
-        }
-        for (double aIH : IH) {
-            IHSum += aIH;
-        }
-        double integralDifference = Math.abs(IHSum - IhSum) / D;
-        System.out.println("\nintegralDifference:\n" + integralDifference);
-        return integralDifference;
-    }
 
-    private double findMaxDifference(double[] Ih, double[] IH) {
-        double max = Math.abs(IH[0] - Ih[0]);
+    private double findRelativeApproximationDifference(double[] Ih, double[] IH) {
+        double[] newIh=reCountApproximation(Ih);
+
+        double max = Math.abs(IH[0] - newIh[0]);
+        for (int i = 1; i < IH.length; i++) {
+            double tempDifference = Math.abs(IH[i] - newIh[i]);
+            if (tempDifference > max) {
+                max = tempDifference;
+            }
+        }
+        return max / D;
+        /*double max = Math.abs(IH[0] - Ih[0]);
         for (int i = 1; i < IH.length; i++) {
             double tempDifference = Math.abs(IH[i] - Ih[STEP_DIVIDER * i]);
             if (tempDifference > max) {
                 max = tempDifference;
             }
         }
-        return max;
+        return max / D;*/
 
+    }
+
+    private double[] reCountApproximation(double[] Ih) {
+        double[] newIh = new double[Ih.length / 2];
+        double previousStep = EntryPoint.STEP * 2;
+        for (double x = EntryPoint.LOWER_LIMIT + 0.5 * previousStep, i = 0; x < EntryPoint.UPPER_LIMIT; i++, x += previousStep) {
+            for (double t = EntryPoint.LOWER_LIMIT + 0.5 * EntryPoint.STEP, j = 0; t < EntryPoint.UPPER_LIMIT; j++, t += EntryPoint.STEP) {
+                newIh[(int) i] += (t + Math.pow(t, 2) * x) * Ih[(int) j];
+            }
+            newIh[(int) i] = newIh[(int) i] * EntryPoint.STEP + 1;
+        }
+        return newIh;
+    }
+
+    private double findFaсtualApproximationDifference(double[] approximateSolution) {
+        double[] exactSolution = new ExactSolution(approximateSolution.length).getExactSolution();
+        double maxDiffer = Math.abs(exactSolution[0] - approximateSolution[0]);
+        for (int i = 1; i < approximateSolution.length; i++) {
+            double tempDiffer = Math.abs(exactSolution[i] - approximateSolution[i]);
+            if (maxDiffer < tempDiffer) {
+                maxDiffer = tempDiffer;
+            }
+        }
+        return maxDiffer;
     }
 }
